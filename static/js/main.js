e1e2b9c1810c604534cd6b6104d772b42b17780a
AlanGcDev <gamebrocoli@gmail.com>
2024-05-08 17:33:36 -0600

    casi termino xd

--- static/js/main.js
+++ static/js/main.js
@@ -51,3 +51,126 @@ document.querySelectorAll('.menu a').forEach(a => {
     document.getElementById('more-scoreboard').style.display = 'block';
     this.style.display = 'none';
   });
+
+  document.getElementById('more-tabs').addEventListener('click', function() {
+    var images = document.querySelectorAll('#tabs-images img');
+    images.forEach(function(image, index) {
+      image.classList.add('animated-image');
+      image.style.animationDelay = (index * 0.5) + 's'; // Ajusta este valor según sea necesario
+      image.style.animationPlayState = 'running';
+    });
+    document.querySelector('#tabs-images').style.display = 'block';
+    document.getElementById('less-tabs').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('less-tabs').addEventListener('click', function() {
+    var images = document.querySelectorAll('#tabs-images img');
+    images.forEach(function(image) {
+      image.classList.remove('animated-image');
+      image.style.animationDelay = '';
+      image.style.animationPlayState = '';
+    });
+    document.querySelector('#tabs-images').style.display = 'none';
+    document.getElementById('more-tabs').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('more-ranks').addEventListener('click', function() {
+    var images = document.querySelectorAll('#ranks-images img');
+    images.forEach(function(image, index) {
+      image.classList.add('animated-image');
+      image.style.animationDelay = (index * 0.5) + 's'; // Ajusta este valor según sea necesario
+      image.style.animationPlayState = 'running';
+    });
+    document.querySelector('#ranks-images').style.display = 'block';
+    document.getElementById('less-ranks').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('less-ranks').addEventListener('click', function() {
+    var images = document.querySelectorAll('#ranks-images img');
+    images.forEach(function(image) {
+      image.classList.remove('animated-image');
+      image.style.animationDelay = '';
+      image.style.animationPlayState = '';
+    });
+    document.querySelector('#ranks-images').style.display = 'none';
+    document.getElementById('more-ranks').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('more-alertas').addEventListener('click', function() {
+    var images = document.querySelectorAll('#alertas-images img');
+    images.forEach(function(image, index) {
+      image.classList.add('animated-image');
+      image.style.animationDelay = (index * 0.5) + 's'; // Ajusta este valor según sea necesario
+      image.style.animationPlayState = 'running';
+    });
+    document.querySelector('#alertas-images').style.display = 'block';
+    document.getElementById('less-alertas').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('less-alertas').addEventListener('click', function() {
+    var images = document.querySelectorAll('#alertas-images img');
+    images.forEach(function(image) {
+      image.classList.remove('animated-image');
+      image.style.animationDelay = '';
+      image.style.animationPlayState = '';
+    });
+    document.querySelector('#alertas-images').style.display = 'none';
+    document.getElementById('more-alertas').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+
+  document.getElementById('more-hologramas').addEventListener('click', function() {
+    var images = document.querySelectorAll('#hologramas-images img');
+    images.forEach(function(image, index) {
+      image.classList.add('animated-image');
+      image.style.animationDelay = (index * 0.5) + 's'; // Ajusta este valor según sea necesario
+      image.style.animationPlayState = 'running';
+    });
+    document.querySelector('#hologramas-images').style.display = 'block';
+    document.getElementById('less-hologramas').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('less-hologramas').addEventListener('click', function() {
+    var images = document.querySelectorAll('#hologramas-images img');
+    images.forEach(function(image) {
+      image.classList.remove('animated-image');
+      image.style.animationDelay = '';
+      image.style.animationPlayState = '';
+    });
+    document.querySelector('#hologramas-images').style.display = 'none';
+    document.getElementById('more-hologramas').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('more-mobs').addEventListener('click', function() {
+    var images = document.querySelectorAll('#mobs-images img');
+    images.forEach(function(image, index) {
+      image.classList.add('animated-image');
+      image.style.animationDelay = (index * 0.5) + 's'; // Ajusta este valor según sea necesario
+      image.style.animationPlayState = 'running';
+    });
+    document.querySelector('#mobs-images').style.display = 'block';
+    document.getElementById('less-mobs').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('less-mobs').addEventListener('click', function() {
+    var images = document.querySelectorAll('#mobs-images img');
+    images.forEach(function(image) {
+      image.classList.remove('animated-image');
+      image.style.animationDelay = '';
+      image.style.animationPlayState = '';
+    });
+    document.querySelector('#mobs-images').style.display = 'none';
+    document.getElementById('more-mobs').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  
