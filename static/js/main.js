b38a29be0aa6c2e3441fa57059dde59053b3b00c
AlanGcDev <gamebrocoli@gmail.com>
2024-05-07 23:40:56 -0600

    Configs

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,50 @@ document.querySelectorAll('.menu a').forEach(a => {
     }
   });
   
+  document.getElementById('more-guis').addEventListener('click', function() {
+    var images = document.querySelectorAll('#guis-images img');
+    images.forEach(function(image, index) {
+      image.classList.add('animated-image');
+      image.style.animationDelay = (index * 0.5) + 's'; // Ajusta este valor según sea necesario
+      image.style.animationPlayState = 'running';
+    });
+    document.querySelector('#guis-images').style.display = 'block';
+    document.getElementById('less-guis').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('less-guis').addEventListener('click', function() {
+    var images = document.querySelectorAll('#guis-images img');
+    images.forEach(function(image) {
+      image.classList.remove('animated-image');
+      image.style.animationDelay = '';
+      image.style.animationPlayState = '';
+    });
+    document.querySelector('#guis-images').style.display = 'none';
+    document.getElementById('more-guis').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('more-scoreboard').addEventListener('click', function() {
+    var images = document.querySelectorAll('#scoreboard-images img');
+    images.forEach(function(image, index) {
+      image.classList.add('animated-image');
+      image.style.animationDelay = (index * 0.5) + 's'; // Ajusta este valor según sea necesario
+      image.style.animationPlayState = 'running';
+    });
+    document.querySelector('#scoreboard-images').style.display = 'block';
+    document.getElementById('less-scoreboard').style.display = 'block';
+    this.style.display = 'none';
+  });
+
+  document.getElementById('less-scoreboard').addEventListener('click', function() {
+    var images = document.querySelectorAll('#scoreboard-images img');
+    images.forEach(function(image) {
+      image.classList.remove('animated-image');
+      image.style.animationDelay = '';
+      image.style.animationPlayState = '';
+    });
+    document.querySelector('#scoreboard-images').style.display = 'none';
+    document.getElementById('more-scoreboard').style.display = 'block';
+    this.style.display = 'none';
+  });
